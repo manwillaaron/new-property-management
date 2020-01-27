e9c809fe953e40953e9d7cb7cc65529a0eb34908
Aaron Manwill <aaronmanwill@Aarons-MacBook.local>
2020-01-27 12:25:22 -0700

    change properties component to functional and added hooks

--- src/components/properties/Properties.js
+++ src/components/properties/Properties.js
@@ -1,126 +1,80 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './Properties.css';
-import {
-  editProperties,
-  addProperty,
-  getProperties
-} from '../../redux/propertiesReducer';
-import { getRenters, deleteRenter } from '../../redux/renterReducer';
+import { getProperties } from '../../redux/propertiesReducer';
+import { getAdmin } from '../../redux/adminReducer';
+import { getRenters } from '../../redux/renterReducer';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
 import Header from '../header/Header';
-import { getAdmin } from '../../redux/adminReducer';
 import RenterDisplay from '../renters/RenterDisplay';
 import SMSForm from '../../SMS/SMSForm';
 
-class Properties extends Component {
-  componentDidUpdate(pp) {
-    if (pp !== this.props || this.props.renters.length === 0) {
-      this.props.getRenters(this.props.match.params.prop_id);
-    }
-  }
+function Properties(props) {
+  useEffect(() => {
+    props.getProperties();
+    props.getAdmin();
+  }, []);
 
-  componentDidMount() {
-    this.props.getProperties();
-    this.props.getAdmin();
-  }
+  useEffect(() => {
+    props.getRenters(props.match.params.prop_id);
+  }, [props.renters]);
 
-  render() {
-    if (!this.props.admin_id) return <Redirect to="/login" />;
-    if (Boolean(this.props.admin.admin.renterCheck) === true)
-      return <Redirect to="/renter" />;
-    let property = this.props.property.find(
-      property => property.prop_id === +this.props.match.params.prop_id
-    );
-    
-    return (
-      <div className="more-info-page" key={property.prop_id}>
-        <div>
-          <Header />
-        </div>
-        <div className="all-prop-info">
-          <div className="property-image-container">
-            <Link
-              to={`/propertyinput/${this.props.match.params.prop_id}`}
-              className="property-image"
-              style={{
-                backgroundImage: 'url(' + `${property.img_url}` + ')',
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat'
-              }}
-            />
-          </div>
-          <div className="prop-info-only">
-            <div className="general-info-items-prop">
-              <h2>Property Name</h2>
-              <h3>{property.property_name}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Address</h2>
-              <h3>{property.address}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Bedrooms</h2>
-              <h3>{property.num_beds}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Bathrooms</h2>
-              <h3>{property.num_baths}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Square Footage</h2>
-              <h3>{property.square_footage}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Acres</h2>
-              <h3>{property.acreage}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Rent</h2>
-              <h3>{property.rent}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Gas Company</h2>
-              <h3>{property.gas_company}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Electric Company</h2>
-              <h3>{property.electric_company}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Occupied</h2>
-              <h3>{JSON.stringify(property.has_renter)}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Fridge?</h2>
-              <h3>{JSON.stringify(property.fridge_included)}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Diswasher?</h2>
-              <h3>{JSON.stringify(property.dishwasher_included)}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Washer and Dryer?</h2>
-              <h3>{JSON.stringify(property.washer_dryer_included)}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Mortgage</h2>
-              <h3>{property.mortgage}</h3>
-            </div>
-            <div className="general-info-items-prop">
-              <h2>Taxes</h2>
-              <h3>{property.tax_yearly}</h3>
-            </div>
-          </div>
-        </div>
-        <div className="renters-smsform">
-          <RenterDisplay prop_id={property.prop_id} />
-          <SMSForm prop_id={property.prop_id} />
-        </div>
+  if (!props.admin_id) return <Redirect to="/login" />;
+  if (Boolean(props.admin.admin.renterCheck) === true)
+    return <Redirect to="/renter" />;
+  let property = props.property.find(
+    property => property.prop_id === +props.match.params.prop_id
+  );
+
+  const propertyVals = [
+    { val: property.property_name, text: 'property_name' },
+    { val: property.address, text: 'address' },
+    { val: property.num_beds, text: 'num_beds' },
+    { val: property.num_baths, text: 'num_baths' },
+    { val: property.square_footage, text: 'square_footage' },
+    { val: property.acreage, text: 'acreage' },
+    { val: property.rent, text: 'rent' },
+    { val: property.gas_company, text: 'gas_company' },
+    { val: property.electric_company, text: 'electric_company' },
+    { val: property.has_renter, text: 'has_renter' },
+    { val: property.fridge_included, text: 'fridge_included' },
+    { val: property.dishwasher_included, text: 'dishwasher_included' },
+    { val: property.washer_dryer_included, text: 'washer_dryer_included' },
+    { val: property.mortgage, text: 'mortgage' },
+    { val: property.tax_yearly, text: 'tax_yearly' }
+  ].map(val => (
+    <div className="general-info-items-prop">
+      <h2>{val.text}</h2>
+      <h3>{val.val}</h3>
+    </div>
+  ));
+
+  return (
+    <div className="more-info-page" key={property.prop_id}>
+      <div>
+        <Header />
       </div>
-    );
-  }
+      <div className="all-prop-info">
+        <div className="property-image-container">
+          <Link
+            to={`/propertyinput/${props.match.params.prop_id}`}
+            className="property-image"
+            style={{
+              backgroundImage: 'url(' + `${property.img_url}` + ')',
+              backgroundPosition: 'center',
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat'
+            }}
+          />
+        </div>
+        <div className="prop-info-only">{propertyVals}</div>
+      </div>
+      <div className="renters-smsform">
+        <RenterDisplay prop_id={property.prop_id} />
+        <SMSForm prop_id={property.prop_id} />
+      </div>
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
@@ -133,10 +87,7 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps, {
-  editProperties,
-  addProperty,
   getProperties,
   getRenters,
-  deleteRenter,
   getAdmin
 })(Properties);
